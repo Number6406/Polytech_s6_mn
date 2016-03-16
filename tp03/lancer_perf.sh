f64e0607fbac9c12113a2f7d164c4625b17e75a1
Alicia Abonnenc <alicia.abonnenc@hotmail.com>
2016-03-16 14:20:13 +0100

    debut tp3

---
+++ tp03/lancer_perf.sh
@@ -0,0 +1,10 @@
+perf stat -e cpu-cycles -e instructions -e cache-references -e cache-misses -e LLC-loads -e LLC-stores --cpu=0--3 --per-core ./prog
+
+pas de openMP
+sans O2 et avec O2
+
+résoudre les systéme linéaires de matrices :
+triangulaire inf
+triangulaire sup
+diagonale
+
