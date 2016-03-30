a68ae3ed910b692373336af2a91510fa574fc538
Alicia Abonnenc <alicia.abonnenc@hotmail.com>
2016-03-30 14:22:29 +0200

    blbl

--- tp03/lancer_perf.sh
+++ tp03/lancer_perf.sh
@@ -1,4 +1,4 @@
-perf stat -e cpu-cycles -e instructions -e cache-references -e cache-misses -e LLC-loads -e LLC-stores --cpu=0--3 --per-core ./prog
+perf stat -e cpu-cycles -e instructions -e cache-references -e cache-misses -e LLC-loads -e LLC-stores --cpu=0-3 --per-core ./prog
 
 pas de openMP
 sans O2 et avec O2
--- tp03/resolution.c
+++ tp03/resolution.c
@@ -6,7 +6,7 @@
 #define BORNESUP 20
 
 
-#define N 9
+#define N 5
 
 /**
  * Définition des différents types de matrices utilisés dans ce programme :
